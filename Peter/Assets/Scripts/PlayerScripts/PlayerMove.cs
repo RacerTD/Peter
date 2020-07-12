d527cc14077e3949c204630e4eb7596a41aa6bbe
Tobias <tobiasdeyle@googlemail.com>
2020-07-12 15:38:45 +0200

    The best movement system we ever created

--- Peter/Assets/Scripts/PlayerScripts/PlayerMove.cs
+++ Peter/Assets/Scripts/PlayerScripts/PlayerMove.cs
@@ -3,57 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMove : Ability
 {
     [Header("Custom Ability Features")]
-
+    private Vector3 moveVector = Vector3.zero;
+    private Vector3 shouldVector = Vector3.zero;
+    [SerializeField] [Range(0, 1)] protected float accelerationSpeed = 0.1f;
     public float MoveSpeed = 2f;
 
     [Header("Sprint")]
     public float SprintSpeed = 4f;
-    [SerializeField]
-    private bool sprintIsToggle = false;
+    [SerializeField] private bool sprintIsToggle = false;
 
     [Header("Sneak")]
     public float SneakSpeed = 1f;
-    [SerializeField]
-    private bool sneakIsToggle = false;
-    private float currentSpeed = 0f;
+    [SerializeField] private bool sneakIsToggle = false;
 
-    #region WalkingType
-    private WalkingType walkingType = WalkingType.Normal;
-    public WalkingType WalkingType
-    {
-        get => walkingType;
-        set
-        {
-            walkingType = value;
-            UpdateWalkingType(value);
-        }
-    }
+    [Header("Jump")]
+    [SerializeField] private Vector3 jumpForce = Vector3.zero;
 
-    private void UpdateWalkingType(WalkingType value)
-    {
-        switch (value)
-        {
-            case WalkingType.Normal:
-                currentSpeed = MoveSpeed;
-                break;
-            case WalkingType.Sprint:
-                currentSpeed = SprintSpeed;
-                break;
-            case WalkingType.Sneak:
-                currentSpeed = SneakSpeed;
-                break;
-        }
-    }
-
-    #endregion
-
-    public void Start()
-    {
-        currentSpeed = MoveSpeed;
-    }
+    public WalkingType WalkingType = WalkingType.Normal;
 
     public void ToggleSprint(InputAction.CallbackContext context)
     {
@@ -107,20 +77,55 @@ public class PlayerMove : Ability
         }
     }
 
+    public void DoJump(InputAction.CallbackContext context)
+    {
+        GetComponent<Rigidbody>().AddForce(jumpForce);
+    }
+
     public override void AbilityStart(InputAction.CallbackContext context)
     {
-        //Debug.Log($"Got new Input {context.started}, {context.performed}, {context.canceled}");
+        UpdateVelocity(context);
+
         base.AbilityStart(context);
     }
 
     public override void AbilityUpdate()
     {
-        if (currentInputAction.started || currentInputAction.performed)
-        {
-            transform.Translate(Vector3.RotateTowards(transform.position, new Vector3(currentInputAction.ReadValue<Vector2>().x * Time.deltaTime * currentSpeed, 0, currentInputAction.ReadValue<Vector2>().y * Time.deltaTime * currentSpeed), float.MaxValue, float.MaxValue));
-        }
+        UpdateVelocity(currentInputAction);
+
+        ApplyVelocity();
 
         base.AbilityUpdate();
+    }
+
+    private void ApplyVelocity()
+    {
+        moveVector += (shouldVector - moveVector) * accelerationSpeed;
+        transform.Translate(Vector3.RotateTowards(transform.position, moveVector * Time.deltaTime, float.MaxValue, float.MaxValue));
+    }
+
+    private void UpdateVelocity(InputAction.CallbackContext context)
+    {
+        if (context.started || context.performed)
+        {
+            switch (WalkingType)
+            {
+                case WalkingType.Normal:
+                    shouldVector = new Vector3(context.ReadValue<Vector2>().x * MoveSpeed, 0, context.ReadValue<Vector2>().y * MoveSpeed);
+                    break;
+                case WalkingType.Sprint:
+                    shouldVector = new Vector3(context.ReadValue<Vector2>().x * MoveSpeed, 0, Mathf.Clamp(context.ReadValue<Vector2>().y * SprintSpeed, -MoveSpeed, SprintSpeed));
+                    break;
+                case WalkingType.Sneak:
+                    shouldVector = new Vector3(context.ReadValue<Vector2>().x * SneakSpeed, 0, context.ReadValue<Vector2>().y * SneakSpeed);
+                    break;
+            }
+        }
+
+        if (context.canceled)
+        {
+            shouldVector = Vector3.zero;
+        }
     }
 }
 
