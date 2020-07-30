3e38fb48c95f2fe043fd2b83bb8c8c29b92b262b
Tobias <tobiasdeyle@googlemail.com>
2020-07-30 18:55:52 +0200

    Combined Enemy Damagable and Better Damagable

--- Peter/Assets/Scripts/Damagable/BetterDamagable.cs
+++ Peter/Assets/Scripts/Damagable/BetterDamagable.cs
@@ -1,11 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BetterDamagable : Damagable
 {
     [Header("Custom Damagable")]
     public GameObject thingToSpawnAtDeath;
+    [SerializeField] protected Slider slider;
+    [SerializeField] protected Gradient gradient;
+    [SerializeField] protected Image fill;
+    private Transform cam;
+
+    protected override void Start()
+    {
+        UpdateMaxHealth();
+        cam = Camera.main.transform;
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        if (slider != null)
+        {
+            slider.transform.LookAt(cam);
+        }
+
+        base.Update();
+    }
+
+    protected override void UpdateHealth()
+    {
+        if (slider != null)
+        {
+            slider.value = Health / MaxHealth;
+            fill.color = gradient.Evaluate(slider.normalizedValue);
+        }
+
+        base.UpdateHealth();
+    }
 
     protected override void OnDeath()
     {
