6e198be39bb82ed6b902b746188571a2a0b7a32a
Tobias <tobiasdeyle@googlemail.com>
2020-07-30 17:38:01 +0200

    Ammo amount goes up as the reload animation plays

--- Peter/Assets/Scripts/PlayerScripts/PlayerReloadWeapon.cs
+++ Peter/Assets/Scripts/PlayerScripts/PlayerReloadWeapon.cs
@@ -11,6 +11,9 @@ public class PlayerReloadWeapon : Ability
     private PlayerShoot playerShoot;
     private PlayerSwitchDimension playerSwitchDimension;
 
+    private float timeBetweenBullets = 0f;
+    private float timeSinceLastBullet = 0f;
+
     protected void Start()
     {
         playerShoot = GetComponent<PlayerShoot>();
@@ -20,14 +23,28 @@ public class PlayerReloadWeapon : Ability
     public override void AbilityStart()
     {
         playerShoot.TimeBlocked = AbilityDuration;
+        playerShoot.Gun.WeaponAnimator.SetTrigger("Idle");
         playerShoot.Gun.WeaponAnimator.SetTrigger("Reload");
+        timeBetweenBullets = (AbilityDuration - 0.1f) / (playerShoot.Gun.MaxGunAmmo - (playerSwitchDimension.DimA ? playerShoot.Gun.CurrentGunAmmoA : playerShoot.Gun.CurrentGunAmmoB));
         base.AbilityStart();
+    }
+
+    public override void AbilityUpdate()
+    {
+        if (timeSinceLastBullet >= timeBetweenBullets)
+        {
+            timeSinceLastBullet = 0f;
+            ReloadOneBullet();
+        }
+
+        timeSinceLastBullet += Time.deltaTime;
+        playerShoot.isAiming = false;
     }
 
     public override void AbilityEnd()
     {
         playerShoot.Gun.WeaponAnimator.SetTrigger("Idle");
-        Reload();
+        //Reload();
         base.AbilityEnd();
     }
 
@@ -63,6 +80,35 @@ public class PlayerReloadWeapon : Ability
                 playerShoot.Gun.CurrentGunAmmoB++;
                 playerShoot.UpdateAmmoDisplay();
             }
+        }
+    }
+
+    /// <summary>
+    /// reloads one bullet
+    /// </summary>
+    private void ReloadOneBullet()
+    {
+        if (playerSwitchDimension.DimA && playerShoot.Gun.CurrentGunAmmoA < playerShoot.Gun.MaxGunAmmo && playerShoot.DimAAmmo > 0)
+        {
+            if (playerShoot.DimAAmmo <= 0)
+            {
+                return;
+            }
+
+            playerShoot.DimAAmmo--;
+            playerShoot.Gun.CurrentGunAmmoA++;
+            playerShoot.UpdateAmmoDisplay();
+        }
+        else if (!playerSwitchDimension.DimA && playerShoot.Gun.CurrentGunAmmoB < playerShoot.Gun.MaxGunAmmo && playerShoot.DimBAmmo > 0)
+        {
+            if (playerShoot.DimBAmmo <= 0)
+            {
+                return;
+            }
+
+            playerShoot.DimBAmmo--;
+            playerShoot.Gun.CurrentGunAmmoB++;
+            playerShoot.UpdateAmmoDisplay();
         }
     }
 }
