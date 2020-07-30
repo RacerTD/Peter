6ab1d7fb1f63b4a065b1fbaaf60ba4ec0eae2cf1
Tobias <tobiasdeyle@googlemail.com>
2020-07-30 17:55:59 +0200

    Added better damagable

---
+++ Peter/Assets/Scripts/Damagable/BetterDamagable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BetterDamagable : Damagable
+{
+    [Header("Custom Damagable")]
+    public GameObject thingToSpawnAtDeath;
+
+    protected override void OnDeath()
+    {
+        if (thingToSpawnAtDeath != null)
+        {
+            Instantiate(thingToSpawnAtDeath, transform.position, transform.rotation);
+        }
+        base.OnDeath();
+    }
+}
