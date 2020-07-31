fe0b3b297161a2c2d2da998ebab7704a755fd6c5
Jennifer <civileey@gmail.com>
2020-07-31 15:24:05 +0200

    damage numbers added (look at new prefabs to make it work)

---
+++ Peter/Assets/Scripts/Damagable/DamageNumbers.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class DamageNumbers : MonoBehaviour
+{
+    public DamageNumbers damageTextParent;
+    public TextMeshProUGUI damageText;
+
+    private void Update()
+    {
+        transform.LookAt(Camera.main.transform);
+    }
+
+    public void SetText(string text)
+    {
+        damageText.text = text;
+    }
+
+    public void CreateFloatingText(string text, Transform location)
+    {
+        Vector3 enemyPosition = new Vector3(location.position.x, location.transform.position.y + Random.Range(-1f, 1f), location.position.z + Random.Range(-1f, 1f));
+        DamageNumbers instance = Instantiate(damageTextParent, enemyPosition, Quaternion.identity, GameManager.Instance.ParticleHolder);
+        instance.SetText(text);
+    }
+}
---
+++ Peter/Assets/Scripts/Damagable/JenniBetterDamagable.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JenniBetterDamagable : Damagable
+{
+    [Header("Custom Damagable")]
+    public GameObject thingToSpawnAtDeath;
+    [SerializeField] protected Slider slider;
+    [SerializeField] protected Gradient gradient;
+    [SerializeField] protected Image fill;
+    [SerializeField] protected DamageNumbers dmgNumbers;
+    private Transform cam;
+
+    protected override void Start()
+    {
+        UpdateMaxHealth();
+        cam = Camera.main.transform;
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        if (slider != null)
+        {
+            slider.transform.LookAt(cam);
+        }
+
+        base.Update();
+    }
+
+    protected override void UpdateHealth()
+    {
+        if (slider != null)
+        {
+            
+            slider.value = Health / MaxHealth;
+            fill.color = gradient.Evaluate(slider.normalizedValue);
+        }
+
+        base.UpdateHealth();
+    }
+
+    protected override void OnDeath()
+    {
+        if (thingToSpawnAtDeath != null)
+        {
+            Instantiate(thingToSpawnAtDeath, transform.position, transform.rotation);
+        }
+        base.OnDeath();
+    }
+
+    public override void DoDamage(float damage)
+    {
+        dmgNumbers.CreateFloatingText(damage.ToString(), transform);
+        base.DoDamage(damage);
+    }
+}
