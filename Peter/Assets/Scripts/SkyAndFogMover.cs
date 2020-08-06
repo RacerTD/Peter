69fbbd38844c5c4c1fca8efaf6d23dfeab53c06c
Tobias <tobiasdeyle@googlemail.com>
2020-08-06 17:12:02 +0200

    Added SkyAndFogMover

---
+++ Peter/Assets/Scripts/SkyAndFogMover.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyAndFogMover : MonoBehaviour
+{
+    private Transform playerTransform;
+
+    private void Start()
+    {
+        playerTransform = GameManager.Instance.CurrentPlayer.transform;
+    }
+
+    private void Update()
+    {
+        transform.position = playerTransform.position;
+    }
+}
