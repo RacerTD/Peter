c0e505d0600dec9db2ed5b710e29f099f1c22fdb
Tobias <tobiasdeyle@googlemail.com>
2020-08-13 16:07:00 +0200

    Updated Ammo script

--- Peter/Assets/Scripts/Ammo.cs
+++ Peter/Assets/Scripts/Ammo.cs
@@ -8,11 +8,12 @@ public class Ammo : MonoBehaviour
     [Range(1, 200)] public int Amount = 10;
     [SerializeField] private Vector3 rotationVector = Vector3.zero;
     [SerializeField] protected VisualEffect onPickUpEffect;
+    [SerializeField] protected bool repositionsAtStart = true;
 
     public void Start()
     {
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, 5f))
+        if (repositionsAtStart && Physics.Raycast(transform.position, Vector3.down, out hit, 5f))
         {
             transform.position = new Vector3(hit.point.x, hit.point.y + 0.1f, hit.point.z);
         }
