2543160712a33ae486d569540978a188f10bb972
Tobias <tobiasdeyle@googlemail.com>
2020-08-14 11:24:12 +0200

    Added Trigger for Unity Events

---
+++ Peter/Assets/Scripts/Interactables/ColliderTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ColliderTrigger : MonoBehaviour
+{
+    public UnityEvent m_OnTriggerEnter = new UnityEvent();
+    public UnityEvent m_OnTriggerExit = new UnityEvent();
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Player>() != null)
+        {
+            m_OnTriggerEnter.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Player>() != null)
+        {
+            m_OnTriggerExit.Invoke();
+        }
+    }
+}
