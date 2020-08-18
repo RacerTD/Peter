638b9baf175fe0c1d8df79e6f513df7e3828b55b
Tobias <tobiasdeyle@googlemail.com>
2020-08-18 14:44:16 +0200

    Added RotationSetter

---
+++ Peter/Assets/Scripts/RotationSetter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationSetter : MonoBehaviour
+{
+    [SerializeField] private Transform objectToRotate;
+    [SerializeField] private Vector3 rotationToSetTo = Vector3.zero;
+    private bool gotSet = false;
+    public void SetRotation()
+    {
+        if (!gotSet)
+        {
+            objectToRotate.rotation = Quaternion.Euler(rotationToSetTo);
+            gotSet = true;
+        }
+    }
+}
